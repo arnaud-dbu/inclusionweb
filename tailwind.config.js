6b5fd2009c0a6368ad426c272eee10e00e7653d2
Arnaud De Burchgraeve <99794461+arnaud-dbu@users.noreply.github.com>
2023-05-08 16:54:36 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      primary: {
+        50: '#fdf8f6',
+        100: '#f2e8e5',
+        200: '#eaddd7',
+        300: '#e0cec7',
+        400: '#CAEBEB',
+        500: '#B6E4E3',
+        600: '#82D3D0',
+        700: '#51C2BE',
+        800: '#1DB1AB',
+        900: '#008E89',
+      }
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
